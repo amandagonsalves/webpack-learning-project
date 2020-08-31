15689a2151a4f16f259a51178b291b994f938320
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-31 17:22:21 -0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const webpack = require('webpack');
 module.exports = {
     mode: "development",
-    entry: './src/main.js'
+    entry: './src/main.js',
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/public'
+    }
 }
